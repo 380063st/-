78a5d49d7d11dd8d8236c898c71836fece68cdaf
Tatyana Savenko <saventanko@gmail.com>
2024-03-17 11:44:54 +0700

    Написала и вывела первый массив строк

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] array1 = new string[5] { "1234", "1567", "-2", "computer science", ":-)" };
+
+            for (int i = 0; i < array1.Length; i++)
+                {
+                    Console.WriteLine(array1[i]);
+                }
+        
+    }
+}
