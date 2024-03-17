405cf10d6d7df0fc33ee1341a57a4354e8dd08a2
Tatyana <130606786+380063st@users.noreply.github.com>
2024-03-17 12:05:19 +0700

    Add files via upload

---
+++ Итоговая/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] array1 = new string[5] { "1234", "1567", "-2", "computer science", ":-)" };
+        string[] array2 = new string[array1.Length];
+
+        void SecondArrayWithIF(string[] array1, string[] array2)
+        {
+            int index = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[index] = array1[i];
+                    index++;
+                }
+            }
+        }
+
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        PrintArray(array1);
+        SecondArrayWithIF(array1, array2);
+        PrintArray(array2);
+    }
+}
